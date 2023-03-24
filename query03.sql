c6c83cb936546dc3a560b879f244f019fcff856b
teresachang11 <thchang@upenn.edu>
2023-03-24 15:29:49 -0400

    Create query03.sql

---
+++ query03.sql
@@ -0,0 +1,14 @@
+SELECT 
+	pwd.address AS address,
+	stops.stop_name AS stop_name,
+	ST_DISTANCE(pwd.geog,stops.geog) AS distance
+FROM phl.pwd_parcels AS pwd
+	CROSS JOIN LATERAL(
+		SELECT 
+		stops.stop_name,
+		stops.geog
+		FROM septa.bus_stops AS stops
+		ORDER BY stops.geog <-> pwd.geog
+		LIMIT 1
+	)stops
+ORDER BY distance desc

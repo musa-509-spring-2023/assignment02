77afd193eb1780ec83471c64197e2e67e8a800be
Myron Bañez <banez@upenn.edu>
2023-03-24 11:36:22 -0400

    updates

--- query05.sql
+++ query05.sql
@@ -3,10 +3,10 @@
 WITH 
 stops AS (
 	SELECT
-		stop.stop_id,
-		stop.wheelchair_boarding,
+		stop.wheelchair_boarding AS wheelchair_boarding,
 		ST_transform(stop.geog::geometry, 2272) AS geog
-	FROM septa.bus_stops AS stop),
+	FROM septa.bus_stops AS stop
+	GROUP BY stop.wheelchair_boarding, geog),
 
 nhood AS (
 	SELECT 
@@ -14,42 +14,31 @@ nhood AS (
 		ST_transform(nhood.geometry, 2272) AS geog
 	FROM azavea.neighborhoods AS nhood),
 
-stops_nhood AS (
+stops_nhood_yes AS (
 	SELECT
 		nhood.neighborhood,
-		stops.stop_id AS stop_id,
-		stops.wheelchair_boarding AS wheelchair_boarding
-	FROM nhood
-	INNER JOIN stops ON st_intersects(stops.geog, nhood.geog)
-	GROUP BY nhood.neighborhood, stops.stop_id, stops.wheelchair_boarding)
-
-SELECT 
-	stops_nhood.neighborhood,
-	count(stops_nhood.stop_id) AS number_of_stops,
-	COUNT(stops_nhood.wheelchair_boarding)  AS number_of_wb
-FROM stops_nhood
-GROUP BY stops_nhood.neighborhood
-
-WITH
-accessible AS (
-	SELECT 
-		COUNT(wheelchair_boarding) AS is_accessible,
-		ST_transform(geog::geometry, 2272) AS geog
-	FROM septa.bus_stops
-	WHERE wheelchair_boarding = 1
-	GROUP BY geog),
+		stops.wheelchair_boarding AS is_accessible
+	FROM stops
+	INNER JOIN nhood ON st_intersects(stops.geog, nhood.geog)
+	WHERE wheelchair_boarding = 1),
 	
-inaccessible AS (
-	SELECT 
-		COUNT(wheelchair_boarding) AS not_accessible,
-		ST_transform(geog::geometry, 2272) AS geog
-	FROM septa.bus_stops
-	WHERE wheelchair_boarding = 2
-	GROUP BY geog)
+stops_nhood_no AS (
+	SELECT
+		nhood.neighborhood,
+		stops.wheelchair_boarding AS not_accessible
+	FROM stops
+	INNER JOIN nhood ON st_intersects(stops.geog, nhood.geog)
+	WHERE wheelchair_boarding = 2 OR wheelchair_boarding = 0),
+	
+final_df AS (
+	SELECT
+		yes_access.neighborhood,
+		yes_access.is_accessible AS num_bus_stops_accessible,
+		no_access.not_accessible AS num_bus_stops_inaccessible
+	FROM stops_nhood_yes AS yes_access
+	INNER JOIN stops_nhood_no AS no_access ON no_access.neighborhood = yes_access.neighborhood
+	GROUP BY yes_access.neighborhood, yes_access.is_accessible, no_access.not_accessible)
 
-SELECT * from inaccessible
--- SELECT * FROM septa.bus_stops
--- bus stops is 8185
--- wheelchair boarding is 8483
--- Join bus stops to neighborhoods || select sum of stops and wheelchair boarding grouped by neighborhood
+SELECT * from stops_nhood_no
 
+ccc

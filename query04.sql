ec9714df092d3fbc91ed809baa36b3e0c9fb9a43
Yesao <673392438@qq.com>
2023-03-27 19:26:05 -0400

    Assignment02 - Shengao Yi

--- query04.sql
+++ query04.sql
@@ -7,8 +7,13 @@ with
 bus_shape_geog as (
     select
         shapes.shape_id,
-        st_makeline(array_agg(st_setsrid(st_makepoint(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326)
-            order by shapes.shape_pt_sequence))::geography as shape_geog
+        st_makeline(
+            array_agg(
+                st_setsrid(
+                    st_makepoint(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326)
+                order by shapes.shape_pt_sequence
+                )
+            )::geography as shape_geog
     from septa.bus_shapes as shapes
     group by shapes.shape_id
 ),

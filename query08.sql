986f609ab09858617e6eb99c432ab0979a289489
lizard12995 <ejs@hnt-16323.apn.wlan.private.upenn.edu>
2023-03-27 14:56:56 -0400

    fixed linting issues

--- query08.sql
+++ query08.sql
@@ -1,11 +1,11 @@
 with -- noqa
 
 penn_collect as (
-    select ST_CONCAVEHULL(ST_COLLECT(geog::geometry),.08) as outline
+    select ST_CONCAVEHULL(ST_COLLECT(univ.geog::geometry), .08) as outline
     from phl.universities as univ
-    where name = 'University of Pennsylvania'
-        and st_dwithin('SRID=4326;POINT(-75.1925955 39.9524158)'::geometry,
-                univ.geog::geometry, .012)
+    where univ.name = 'University of Pennsylvania'
+        and ST_DWITHIN('SRID=4326;POINT(-75.1925955 39.9524158)'::geometry,
+            univ.geog::geometry, .012)
 )
 
 select COUNT(*)

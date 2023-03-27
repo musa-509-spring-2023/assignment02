fb8f77230cd8575b8d820493422e68833f8fdc14
YuhaoChrisJ <yuhaojia98@g.ucla.edu>
2023-03-27 19:53:00 -0400

    change

--- query02.sql
+++ query02.sql
@@ -1,18 +1,18 @@
-/*
-  Which bus stop has the largest population within 800 meters? As a rough
-  estimation, consider any block group that intersects the buffer as being part
-  of the 800 meter buffer.
-*/
-
 with
+ph_bg as (
+	select *
+	from census.blockgroups_2020
+	where statefp = '42' and countyfp = '101'
+),
+
 
 septa_bus_stop_blockgroups as (
     select
         stops.stop_id,
-        '1500000US' || bg.geoid as geoid
+        '1500000US' || ph_bg.geoid as geoid
     from septa.bus_stops as stops
-    inner join census.blockgroups_2020 as bg
-        on st_dwithin(stops.geog, st_setsrid(bg.geog::geography, 4326), 800)
+    inner join ph_bg
+        on st_dwithin(stops.geog, st_setsrid(ph_bg.geog::geography, 4326), 800)
 ),
 
 septa_bus_stop_surrounding_population as (
@@ -30,5 +30,6 @@ select
     stops.geog
 from septa_bus_stop_surrounding_population as pop
 inner join septa.bus_stops as stops using (stop_id)
-order by pop.estimated_pop_800m desc
+where pop.estimated_pop_800m>=500
+order by pop.estimated_pop_800m
 limit 8

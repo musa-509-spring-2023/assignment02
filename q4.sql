e0505e6d30d803c0bcf029267cd268717ebdcdd2
YU WANG <112767640+759257989@users.noreply.github.com>
2023-03-27 18:04:33 -0400

    Update q4.sql

--- q4.sql
+++ q4.sql
@@ -1,12 +1,18 @@
-select
-    sbt.route_id as route_short_name,
-    sbt.trip_headsign as trip_headsign,
-    ST_LENGTH(ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shape_pt_lon, shape_pt_lat), 4326) order by shape_pt_sequence))) as shape_length,
-    ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shape_pt_lon, shape_pt_lat), 4326) order by shape_pt_sequence)) as shape_geog
+WITH leng AS (
+	select 
+	DISTINCT shape_id,
+	ST_LENGTH(ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shape_pt_lon, shape_pt_lat), 4326) order by shape_pt_sequence))::geography)as shape_length,
+	ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shape_pt_lon, shape_pt_lat), 4326) order by shape_pt_sequence))::geography as shape_geog
+	from septa.bus_shapes
+	group by shape_id
+	ORDER BY shape_length DESC )
 
+select
+    DISTINCT sbt.route_id as route_short_name,
+    sbt.trip_headsign as trip_headsign,
+    leng.shape_geog as shape_geog,
+ 	leng. shape_length as shape_length
 from septa.bus_trips as sbt
-inner join septa.bus_shapes as sbs
-    on sbt.shape_id = sbs.shape_id
-group by route_short_name, trip_headsign
+join leng on sbt.shape_id = leng.shape_id
 order by shape_length desc
 limit 2

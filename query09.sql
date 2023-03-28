39e1edd498ac2f8769f59c2de3e0af21f3a89ad9
teresachang11 <thchang@upenn.edu>
2023-03-28 17:15:54 -0400

    Update query09.sql

--- query09.sql
+++ query09.sql
@@ -1,8 +1,7 @@
-SELECT bg.geoid
-FROM census.blockgroups_2020 AS bg
-JOIN (
-  SELECT *
-  FROM phl.pwd_parcels
-  WHERE address = '3401-39 WALNUT ST'
-) AS pwd
-ON ST_Intersects(ST_TRANSFORM(bg.geog::geometry, 4326), ST_TRANSFORM(pwd.geog::geometry,4326))
+WITH pwd_parcels AS (
+    SELECT * FROM phl.pwd_parcels
+    WHERE address = '3401-39 WALNUT ST'
+)
+
+SELECT bg.geoid FROM census.blockgroups_2020 AS bg
+JOIN pwd_parcels AS pwd ON ST_INTERSECTS(ST_TRANSFORM(bg.geog::geometry, 4326), ST_TRANSFORM(pwd.geog::geometry, 4326))

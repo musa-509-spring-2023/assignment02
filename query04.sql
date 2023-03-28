22478a86dd6855236cb4b5f781bb6ac5566d979d
kmu973 <kmu973@Naver.com>
2023-03-28 18:50:05 -0400

    query04 linting

--- query04.sql
+++ query04.sql
@@ -19,45 +19,50 @@ septa.bus_trips
 
 
 WITH
+  -- create bus route geography
+  bus_shape_geog AS (
+    SELECT
+      shape_id,
+      ST_MakeLine(
+        ARRAY_AGG(
+          ST_SetSRID(ST_MakePoint(shape_pt_lon, shape_pt_lat), 4326)
+          ORDER BY shape_pt_sequence
+        )
+      )::geography AS shape_geog
+    FROM septa.bus_shapes
+    GROUP BY shape_id
+  ),
 
--- create bus route geography
+  -- calculate route length
+  bus_shape_geog_length AS (
+    SELECT
+      *,
+      ST_Length(bus_shape_geog.shape_geog) AS shape_length
+    FROM bus_shape_geog
+  ),
 
-bus_shape_geog AS (
-SELECT shape_id, ST_MakeLine(array_agg(
-  ST_SetSRID(ST_MakePoint(shape_pt_lon, shape_pt_lat),4326) ORDER BY shape_pt_sequence))::geography AS shape_geog
-FROM septa.bus_shapes
-GROUP BY shape_id
-),
+  -- combine with trips
+  bus_shape_geog_length_trips AS (
+    SELECT *
+    FROM bus_shape_geog_length AS shape
+    INNER JOIN septa.bus_trips AS trips USING (shape_id)
+  ),
 
--- calculate route length
+  -- combine with routes to get route name
+  last_result AS (
+    SELECT
+      b.route_short_name,
+      a.trip_headsign,
+      a.shape_length,
+      a.shape_geog
+    FROM bus_shape_geog_length_trips AS a
+    INNER JOIN septa.bus_routes AS b USING (route_id)
+    ORDER BY a.shape_length DESC
+  )
 
-bus_shape_geog_length AS (
-SELECT *, ST_Length(bus_shape_geog.shape_geog) AS shape_length
-FROM bus_shape_geog
-),
-
--- combine with trips
-
-bus_shape_geog_length_trips AS (
-SELECT * 
-FROM bus_shape_geog_length as shape
-INNER JOIN septa.bus_trips as trips using (shape_id)
-),
-
--- combine with routes to get route name
-
-last_result AS (
-	
-SELECT b.route_short_name, a.trip_headsign, a.shape_length, a.shape_geog
-FROM bus_shape_geog_length_trips AS a
-INNER JOIN septa.bus_routes AS b using (route_id)
-ORDER BY a.shape_length DESC
-
-) 
-
--- combine with routes to get route name
-
+-- select the two routes with the longest trips
 SELECT DISTINCT *
 FROM last_result
 ORDER BY shape_length DESC
-LIMIT 2
+LIMIT 2;
+

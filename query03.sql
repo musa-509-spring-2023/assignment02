c0fea300af63cc4195eaee5f79994d00deb642ba
teresachang11 <thchang@upenn.edu>
2023-03-28 18:09:08 -0400

    Update query03.sql

--- query03.sql
+++ query03.sql
@@ -6,9 +6,10 @@ FROM phl.pwd_parcels AS pwd
 CROSS JOIN LATERAL (
     SELECT
         stops.stop_name,
-        stops.geog
+        stops.geog,
+        stops.geog <-> pwd.geog AS nn
     FROM septa.bus_stops AS stops
-    ORDER BY stops.geog < - > pwd.geog
+    ORDER BY stops.nn
     LIMIT 1
 ) AS stops
 ORDER BY distance DESC

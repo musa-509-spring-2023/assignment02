8aa5ff028a79e4b125bf308bd7d442e445c34da0
kmu973 <kmu973@Naver.com>
2023-03-28 19:05:08 -0400

    query02

--- query02.sql
+++ query02.sql
@@ -3,38 +3,42 @@
  */
 
 WITH
-  philly_blockgroups AS (
+philly_blockgroups AS (
     SELECT
-      geoid,
-      geog,
-      countyfp
+        bg.geoid,
+        bg.geog,
+        bg.countyfp
     FROM census.blockgroups_2020 AS bg
     WHERE bg.countyfp = '42101'
-  ),
-  septa_bus_stop_blockgroups AS (
+),
+
+septa_bus_stop_blockgroups AS (
     SELECT
-      stops.stop_id,
-      bg.geoid
+        stops.stop_id,
+        bg.geoid
     FROM septa.bus_stops AS stops
     INNER JOIN philly_blockgroups AS bg
-      ON ST_DWithin(ST_SetSRID(stops.geog::geography, 4326), ST_SetSRID(bg.geog::geography, 4326), 800)
-  ),
-  septa_bus_stop_surrounding_population AS (
+        ON ST_DWithin(ST_SetSRID(stops.geog::geography, 4326), ST_SetSRID(bg.geog::geography, 4326), 800)
+),
+
+septa_bus_stop_surrounding_population AS (
     SELECT
-      stops.stop_id,
-      SUM(pop.total) AS estimated_pop_800m
+        stops.stop_id,
+        SUM(pop.total) AS estimated_pop_800m
     FROM septa_bus_stop_blockgroups AS stops
     INNER JOIN census.population_2020 AS pop
-      ON stops.geoid = pop.geoid
+        ON stops.geoid = pop.geoid
     GROUP BY stops.stop_id
-  )
+)
+
 SELECT
-  stops.stop_name,
-  pop.estimated_pop_800m,
-  stops.geog
+    stops.stop_name,
+    pop.estimated_pop_800m,
+    stops.geog
 FROM septa_bus_stop_surrounding_population AS pop
 INNER JOIN septa.bus_stops AS stops
-  ON pop.stop_id = stops.stop_id
+    ON pop.stop_id = stops.stop_id
 WHERE pop.estimated_pop_800m > 500
 ORDER BY pop.estimated_pop_800m
 LIMIT 8;
+

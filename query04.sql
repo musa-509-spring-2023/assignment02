7c8f13a7d49d9f61ec00d94fd25ca942dc835a32
teresachang11 <thchang@upenn.edu>
2023-03-28 17:15:41 -0400

    Update query04.sql

--- query04.sql
+++ query04.sql
@@ -1,22 +1,19 @@
-WITH length AS(
-SELECT 
-	shape_id, 
-	ST_MakeLine(array_agg(
-  		ST_SetSRID(ST_MakePoint(shape_pt_lon, shape_pt_lat),4326) ORDER BY shape_pt_sequence)) as shape_geog,
-  	ST_LENGTH(ST_MakeLine(array_agg(
-  		ST_SetSRID(ST_MakePoint(shape_pt_lon, shape_pt_lat),4326) ORDER BY shape_pt_sequence))) as shape_length
-FROM septa.bus_shapes
-GROUP BY shape_id
-
+WITH length AS (
+    SELECT
+        shape_id,
+        ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shape_pt_lon, shape_pt_lat), 4326) ORDER BY shape_pt_sequence)) AS shape_geog,
+        ST_LENGTH(ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shape_pt_lon, shape_pt_lat), 4326) ORDER BY shape_pt_sequence))) AS shape_length
+    FROM septa.bus_shapes
+    GROUP BY shape_id
 )
 
-SELECT DISTINCT 
-	trips.trip_headsign,
-	routes.route_short_name,
-	length.shape_geog,
-	length.shape_length
+SELECT DISTINCT
+    trips.trip_headsign,
+    routes.route_short_name,
+    length.shape_geog,
+    length.shape_length
 FROM length
 JOIN septa.bus_trips AS trips ON length.shape_id = trips.shape_id
-JOIN septa.bus_routes AS routes ON routes.route_id= trips.route_id
+JOIN septa.bus_routes AS routes ON routes.route_id = trips.route_id
 ORDER BY shape_length DESC
 LIMIT 2

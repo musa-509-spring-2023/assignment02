493ab6d1c20ca455cc938df3533fe9ae2eba8be0
teresachang11 <thchang@upenn.edu>
2023-03-28 18:09:20 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -3,7 +3,7 @@ WITH wheelchair_boarding AS (
         nbh.name,
         COUNT(stops.stop_id) AS count
     FROM azavea.neighborhoods AS nbh
-    JOIN septa.bus_stops AS stops ON ST_INTERSECTS(stops.geog, nbh.geog)
+    INNER JOIN septa.bus_stops AS stops ON ST_INTERSECTS(stops.geog, nbh.geog)
     WHERE stops.wheelchair_boarding = 1
     GROUP BY nbh.name
 ),
@@ -12,19 +12,18 @@ ratio AS (
     SELECT
         nbh.name,
         COUNT(stops.stop_id)::FLOAT AS tot_stops,
-        w.count / COUNT(stops.stop_id)::FLOAT AS stop_ratio,
-        (w.count / nbh.shape_area) * 10 ^ 5 / 0.787 AS area_ratio
+        (w.count / COUNT(stops.stop_id)::FLOAT) * 0.7 + ((w.count / nbh.shape_area) * 10 ^ 5 / 0.787) * 0.3 AS accessibility_metric
     FROM azavea.neighborhoods AS nbh
-    JOIN septa.bus_stops AS stops ON ST_INTERSECTS(stops.geog, nbh.geog)
+    INNER JOIN septa.bus_stops AS stops ON ST_INTERSECTS(stops.geog, nbh.geog)
     LEFT JOIN wheelchair_boarding AS w ON nbh.name = w.name
     GROUP BY nbh.name, w.count, nbh.shape_area
 )
 
 SELECT
     r.name AS neighborhood_name,
-    ((r.stop_ratio * 0.7) + (r.area_ratio * 0.3)) AS accessibility_metric,
+    r.accessibility_metric,
     w.count AS num_bus_stops_accessible,
     r.tot_stops - w.count AS num_bus_stops_inaccessible
 FROM ratio AS r
-JOIN wheelchair_boarding AS w ON w.name = r.name
-ORDER BY accessibility_metric DESC
+INNER JOIN wheelchair_boarding AS w ON w.name = r.name
+ORDER BY r.accessibility_metric DESC

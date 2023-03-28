255417b54c65416a7316aaba88e9f4e7af48e88a
teresachang11 <thchang@upenn.edu>
2023-03-28 17:15:52 -0400

    Update query08.sql

--- query08.sql
+++ query08.sql
@@ -1,6 +1,3 @@
 SELECT COUNT(*) AS count_block_groups
 FROM census.blockgroups_2020 AS bg
-WHERE ST_WITHIN(ST_TRANSFORM(bg.geog::geometry,4326), 
-				ST_TRANSFORM((SELECT geog::geometry FROM azavea.neighborhoods WHERE name = 'UNIVERSITY_CITY')::geometry, 4326))
-
-	
+WHERE ST_WITHIN(ST_TRANSFORM(bg.geog::geometry, 4326), ST_TRANSFORM((SELECT geog::geometry FROM azavea.neighborhoods WHERE name = 'UNIVERSITY_CITY')::geometry, 4326))

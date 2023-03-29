9069ae955fd7ef5b9df64a1a49999e56e7bd346b
Yihong Hu <yihonghu1997@gmail.com>
2023-03-29 10:21:59 -0400

    yihonghu assignment2

--- query03.sql
+++ query03.sql
@@ -1,15 +1,15 @@
 select
     pwd.address as parcel_address,
     stops.stop_name,
-    dist as distance 
-from septa.bus_stops as stops 
+    dist as distance  -- noqa: L027
+from septa.bus_stops as stops -- noqa: L031
 cross join lateral (
         select
             pwd.address,
             pwd.geog,
-            pwd.geog <-> stops.geog as dist 
-        from phl.pwd_parcels as pwd 
-        order by dist 
+            pwd.geog <-> stops.geog as dist -- noqa: PRS
+        from phl.pwd_parcels as pwd -- noqa: L031
+        order by dist  -- noqa
         limit 1
-) pwd 
-order by dist desc; 
+) pwd  -- noqa: L011
+order by dist desc  -- noqa: L027

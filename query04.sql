62f2091414d8c985a28c9a6b3abe66a59de7c652
Sofia <sfasull1@binghamton.edu>
2023-04-16 22:03:13 -0400

    update 4

--- query04.sql
+++ query04.sql
@@ -2,26 +2,44 @@ CREATE INDEX bus_routes_geog_idx ON phl.pwd_parcels USING gist (geog);
 CREATE INDEX bus_stops_geog_idx ON septa.bus_stops USING gist (geog);
 
 
-
-WITH trip_info AS (
+WITH
+the_route AS (
     SELECT
-        bus.route_id AS bus_route_id,
-        trips.route_id AS trips_route_id,
-        bus.route_short_name,
-        trips.trip_headsign,
-        trips.shape_id
-    FROM septa.bus_routes AS bus
-    JOIN septa.bus_trips AS trips USING (route_id)
+        shapes.shape_id,
+        ST_MAKELINE(ARRAY_AGG(ST_SETSRID(
+            ST_MAKEPOINT(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326
+        )
+        ORDER BY shapes.shape_pt_sequence)) AS shape_geog
+    FROM septa.bus_shapes AS shapes
+    GROUP BY shapes.shape_id
+),
+
+route_length AS (
+    SELECT
+        the_route.shape_id,
+        the_route.shape_geog,
+        ST_LENGTH(the_route.shape_geog) AS shape_length
+    FROM the_route
+),
+
+final_table AS (
+    SELECT
+        routes.route_short_name,
+        shape_id,
+        route_length.shape_length,
+        bus_trips.trip_headsign,
+        route_length.shape_geog,
+        route_id
+    FROM route_length
+    INNER JOIN septa.bus_trips USING (shape_id)
+    INNER JOIN septa.bus_routes AS routes USING (route_id)
 )
 
-SELECT
-    trip_info.route_short_name,
-    trip_info.trip_headsign,
-    shapes.shape_id,
-    ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326) ORDER BY shapes.shape_pt_sequence)) AS shape_geog,
-    ST_LENGTH(ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326) ORDER BY shapes.shape_pt_sequence))) AS shape_length
-FROM septa.bus_shapes AS shapes
-LEFT JOIN trip_info ON trip_info.shape_id = shapes.shape_id
-GROUP BY shapes.shape_id, trip_info.route_short_name, trip_info.trip_headsign
+SELECT DISTINCT
+    route_short_name,
+    trip_headsign,
+    shape_geog,
+    shape_length
+FROM final_table
 ORDER BY shape_length DESC
 LIMIT 2

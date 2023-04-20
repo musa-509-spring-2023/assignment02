87b35bd830f7eded848bdf7b9ce1ef76603cbefc
xueningz22 <xueningzhang22@gmail.com>
2023-04-20 12:27:14 -0400

    ASSIGNMENT2

--- query10.sql
+++ query10.sql
@@ -8,21 +8,21 @@ SELECT
         WHEN ST_WITHIN(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), bu.geometry)
             THEN
             -- Get the nearest address to the rail stop
-            CONCAT(
-                ROUND(ST_DISTANCE(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), pp.geometry))::integer,
-                ' meters ',
-                CASE
-                    WHEN ST_AZIMUTH(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), pp.geometry) BETWEEN 0 AND 90 THEN 'NE'
-                    WHEN ST_AZIMUTH(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), pp.geometry) BETWEEN 90 AND 180 THEN 'SE'
-                    WHEN ST_AZIMUTH(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), pp.geometry) BETWEEN 180 AND 270 THEN 'SW'
-                    ELSE 'NW'
-                END,
-                ' of ',
-                pp."ADDRESS",
-                ' in ',
-                n.name,
-                ', University of Pennsylvania'
-            )
+                CONCAT(
+                    ROUND(ST_DISTANCE(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), pp.geometry))::integer,
+                    ' meters ',
+                    CASE
+                        WHEN ST_AZIMUTH(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), pp.geometry) BETWEEN 0 AND 90 THEN 'NE'
+                        WHEN ST_AZIMUTH(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), pp.geometry) BETWEEN 90 AND 180 THEN 'SE'
+                        WHEN ST_AZIMUTH(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), pp.geometry) BETWEEN 180 AND 270 THEN 'SW'
+                        ELSE 'NW'
+                    END,
+                    ' of ',
+                    pp."ADDRESS",
+                    ' in ',
+                    n.name,
+                    ', University of Pennsylvania'
+                )
         ELSE
             -- Get the nearest address to the rail stop
             CONCAT(
@@ -58,7 +58,7 @@ INNER JOIN (
         p.geometry,
         p."ADDRESS",
         n.name
-    ) AS ppn ON ST_DWITHIN(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), ppn.geometry, ppn.distance)
+) AS ppn ON ST_DWITHIN(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), ppn.geometry, ppn.distance)
 INNER JOIN phl.pwd_parcels AS pp ON ppn.geometry = pp.geometry
 INNER JOIN azavea.neighborhoods AS n ON ppn.name = n.name
 INNER JOIN census.block_upenn_2020 AS bu ON ST_INTERSECTS(ST_MAKEPOINT(rs.stop_lon, rs.stop_lat), bu.geometry)

acee96526c7f98a992bae9605ca7589437e52fe6
AcresHectares <arloandacres@gmail.com>
2023-05-05 03:15:41 -0400

    modified:   query04.sql

--- query04.sql
+++ query04.sql
@@ -1,42 +1,42 @@
 WITH bus_length AS (
-    SELECT
-        shapes.shape_id AS shape_id,
-        ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326) ORDER BY shapes.shape_pt_sequence)) AS shape_geog,
-        ST_LENGTH(ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326) ORDER BY shapes.shape_pt_sequence))) AS shape_length
-    FROM septa.bus_shapes AS shapes
-    GROUP BY shapes.shape_id
+    SELECT
+        shapes.shape_id AS shape_id,
+        ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326) ORDER BY shapes.shape_pt_sequence)) AS shape_geog,
+        T_LENGTH(ST_MAKELINE(ARRAY_AGG(ST_SETSRID(ST_MAKEPOINT(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326) ORDER BY shapes.shape_pt_sequence))) AS shape_length
+    FROM septa.bus_shapes AS shapes
+    GROUP BY shapes.shape_id
 ),
 
 trips AS (
-    SELECT
-        bus_trips.trip_headsign,
-        bus_trips.shape_id,
-        bus_trips.route_id,
-        bus_length.shape_length,
-        bus_length.shape_geog,
-        bus_length.shape_id
-    FROM bus_length
-    INNER JOIN septa.bus_trips
-        ON (bus_length.shape_id = septa.bus_trips.shape_id)
+    SELECT
+        bus_trips.trip_headsign,
+        bus_trips.shape_id,
+        bus_trips.route_id,
+        bus_length.shape_length,
+        bus_length.shape_geog,
+        bus_length.shape_id
+    FROM bus_length
+    INNER JOIN septa.bus_trips
+        ON (bus_length.shape_id = septa.bus_trips.shape_id)
 ),
 
 lastt AS (
-    SELECT
-        routes.route_short_name,
-        trips.trip_headsign,
-        trips.shape_length,
-        trips.shape_geog,
-        trips.route_id
-    FROM trips
-    INNER JOIN septa.bus_routes AS routes
-        ON (trips.route_id = routes.route_id)
+    SELECT
+        routes.route_short_name,
+        trips.trip_headsign,
+        trips.shape_length,
+        trips.shape_geog,
+        trips.route_id
+    FROM trips
+    INNER JOIN septa.bus_routes AS routes
+        ON (trips.route_id = routes.route_id)
 )
 
 SELECT DISTINCT
-    lastt.route_short_name,
-    lastt.trip_headsign,
-    lastt.shape_length,
-    lastt.shape_geog
+    lastt.route_short_name,
+    lastt.trip_headsign,
+    lastt.shape_length,
+    lastt.shape_geog
 FROM lastt
 ORDER BY lastt.shape_length DESC
 LIMIT 2

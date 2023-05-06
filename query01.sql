c35fcb74f5718378f25cf0e73c83e8fa471f4f8e
Yifei SUN <100406743+miaomiao612@users.noreply.github.com>
2023-05-06 17:11:09 -0400

    Add files via upload

---
+++ query01.sql
@@ -0,0 +1,28 @@
+with
+
+septa_bus_stop_blockgroups as (
+    select
+        stops.stop_id,
+        '1500000US' || bg.geoid as geoid
+    from septa.bus_stops as stops
+    inner join census.blockgroups_2020 as bg
+        on st_dwithin(stops.geography, bg.geog, 800)
+),
+
+septa_bus_stop_surrounding_population as (
+    select
+        stops.stop_id,
+        sum(pop.total) as estimated_pop_800m
+    from septa_bus_stop_blockgroups as stops
+    inner join census.population_2020 as pop using (geoid)
+    group by stops.stop_id
+)
+
+select
+    stops.stop_name,
+    pop.estimated_pop_800m,
+    stops.geography
+from septa_bus_stop_surrounding_population as pop
+inner join septa.bus_stops as stops using (stop_id)
+order by pop.estimated_pop_800m desc
+limit 8
---
+++ query04.sql
@@ -0,0 +1,43 @@
+with
+
+bus_shape_geog as (
+    select
+        shapes.shape_id,
+        st_makeline(array_agg(st_setsrid(
+            st_makepoint(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326
+        )
+        order by shapes.shape_pt_sequence)) as shape_geog
+    from septa.bus_shapes as shapes
+    group by shapes.shape_id
+),
+
+bus_shape_geog_length as (
+    select
+        bus_shape_geog.shape_id,
+        bus_shape_geog.shape_geog,
+        st_length(bus_shape_geog.shape_geog) as shape_length
+    from bus_shape_geog
+),
+
+bus_shape_trips as (
+    select
+        routes.route_short_name,
+        shape_id,
+        bus_shape_geog_length.shape_length,
+        bus_trips.trip_headsign,
+        bus_shape_geog_length.shape_geog,
+        route_id
+    from bus_shape_geog_length
+    inner join septa.bus_trips using (shape_id)
+    inner join septa.bus_routes as routes using (route_id)
+)
+
+select distinct
+    route_short_name,
+    trip_headsign,
+    shape_geog,
+    shape_length
+from bus_shape_trips
+order by shape_length desc
+limit 2
+

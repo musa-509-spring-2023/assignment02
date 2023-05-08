778d1ed07791f1bfd8744cc8f886d1f78da4bdad
Sofia <sfasull1@binghamton.edu>
2023-05-08 19:50:12 -0400

    rest of queries

---
+++ query09.sql
@@ -0,0 +1,11 @@
+WITH meyerson_parcels AS (
+    SELECT
+    parcels.geog
+    FROM phl.pwd_parcels AS parcels
+    WHERE parcels.address = '220-30 S 34TH ST'
+)
+
+SELECT bg.geoid AS geo_id
+FROM census.blockgroups_2020 bg
+    JOIN meyerson_parcels
+        ON ST_INTERSECTS(bg.geog, meyerson_parcels.geog)

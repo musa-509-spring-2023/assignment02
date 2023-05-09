69a3654fd64dc01ce878fddd2436f293eaa2f68b
Yihong Hu <yihonghu1997@gmail.com>
2023-05-08 21:36:20 -0400

    added linting rules and switched positions

--- query03.sql
+++ query03.sql
@@ -1,15 +1,16 @@
 select
     pwd.address as parcel_address,
     stops.stop_name,
-    dist as distance  -- noqa: L027
-from septa.bus_stops as stops -- noqa: L031
+    st_distance(pwd.geog, stops.geog) as distance  -- noqa: L027
+from phl.pwd_parcels as pwd -- noqa: L031
 cross join lateral (
         select
-            pwd.address,
-            pwd.geog,
-            pwd.geog <-> stops.geog as dist -- noqa: PRS
-        from phl.pwd_parcels as pwd -- noqa: L031
+            stops.stop_name,
+            stops.geog,
+            stops.geog <-> pwd.geog as dist -- noqa: PRS
+        from septa.bus_stops as stops -- noqa: L031
         order by dist  -- noqa
         limit 1
-) pwd  -- noqa: L011
-order by dist desc  -- noqa: L027
+) stops -- noqa: L011
+order by dist desc;
+
